ad9dec2b7d135bde6bee56a505a668f3ef485232
OrlovaSophya <22122018sofa02@gmail.com>
2025-05-01 13:05:58 +0300

    Создан слайдер

--- scripts/script.js
+++ scripts/script.js
@@ -21,3 +21,42 @@ intensiveImg.addEventListener('mouseenter', () => {
 });
 
 });
+
+// Создание слайдера
+let currentIndex = 0; 
+const slider = document.querySelectorAll(".news__card");
+const prevButton = document.querySelector(".news__left");
+const nextButton = document.querySelector(".news__right");
+const visibleCards = 3;
+updateSlider();
+
+if (prevButton){
+prevButton.addEventListener("click", () =>{
+    if (currentIndex > 0) {
+        currentIndex--;
+    } else {
+        currentIndex = slider.length - visibleCards;
+    }
+    updateSlider();
+});
+}
+if (nextButton){
+nextButton.addEventListener("click", () =>{
+    if (currentIndex < slider.length - visibleCards) {
+        currentIndex++;
+    } else {
+        currentIndex = 0;
+    }
+    updateSlider();
+});
+}
+function updateSlider() {
+    slider.forEach((item, index)=>{
+        if (index >= currentIndex && index < currentIndex + visibleCards) {
+            item.style.display = "flex";
+        } else {
+            item.style.display = "none";
+        }
+    });
+}
+
